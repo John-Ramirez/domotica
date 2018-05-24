1d57dfbefe7f1818b2e00c145d1a248cd04e856d
John-Ramirez <jhonixt321@gmail.com>
2018-05-24 15:26:12 -0500

    1.1
    Coreccion de errores de compilacion y error de licencia

--- database/setup.sql
+++ database/setup.sql
@@ -1,2 +1,2 @@
 create table sensores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, temperatura TEXT, motor TEXT);
-insert into directory (temperatura, motor) values ('24', 'on');
+insert into sensores (temperatura, motor) values ('24', 'on');
