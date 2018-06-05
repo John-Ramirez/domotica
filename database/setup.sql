44bda5b39e2e39a88a93b2ebf96f474931479ec4
John-Ramirez <jhonixt321@gmail.com>
2018-06-05 10:31:32 -0500

    1.11
    Nuevos campos en la tabla sensnores y nuevas tablas en la base de datos domotica: estados y lcd.

--- database/setup.sql
+++ database/setup.sql
@@ -1,2 +1,7 @@
-create table sensores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, temperatura TEXT, motor TEXT);
-insert into sensores (temperatura, motor) values ('24', 'on');
+create table sensores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, temperatura TEXT, fotoresistor TEXT, pir TEXT);
+create table estados (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, servouno TEXT, servodos TEXT, motor TEXT, led TEXT, boton TEXT);
+create table lcd (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, textouno TEXT, textodos TEXT);
+/* insertar primeros registros de prueba*/
+insert into sensores (temperatura, fotoresistor, pir) values ('24', '132', 'true');
+insert into estados (servouno, servodos, motor, led, boton) values ('off', 'off', 'off', 'off', 'off');
+insert into lcd (textouno, textodos) values ('Temperatura', '23');	
